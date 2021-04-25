88c77fba80dc1007ebde24d9f8ce6aac7eaf7780
Guilherme Toshio Iita <78798399+GuilhermeToshio@users.noreply.github.com>
2021-04-25 18:41:58 -0300

    Add files via upload

---
+++ Atividade 6/frmExercicio3.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Teste_de_Metodos
+{
+    partial class frmExercicio3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPalavra1 = new System.Windows.Forms.Label();
+            this.lblPalavra2 = new System.Windows.Forms.Label();
+            this.txtPalavra1 = new System.Windows.Forms.TextBox();
+            this.txtPalavra2 = new System.Windows.Forms.TextBox();
+            this.btnRemover1 = new System.Windows.Forms.Button();
+            this.bntRemover2 = new System.Windows.Forms.Button();
+            this.btnInverte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPalavra1
+            // 
+            this.lblPalavra1.AutoSize = true;
+            this.lblPalavra1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPalavra1.Location = new System.Drawing.Point(118, 92);
+            this.lblPalavra1.Name = "lblPalavra1";
+            this.lblPalavra1.Size = new System.Drawing.Size(86, 24);
+            this.lblPalavra1.TabIndex = 0;
+            this.lblPalavra1.Text = "Palavra 1";
+            // 
+            // lblPalavra2
+            // 
+            this.lblPalavra2.AutoSize = true;
+            this.lblPalavra2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPalavra2.Location = new System.Drawing.Point(118, 178);
+            this.lblPalavra2.Name = "lblPalavra2";
+            this.lblPalavra2.Size = new System.Drawing.Size(86, 24);
+            this.lblPalavra2.TabIndex = 1;
+            this.lblPalavra2.Text = "Palavra 2";
+            // 
+            // txtPalavra1
+            // 
+            this.txtPalavra1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPalavra1.Location = new System.Drawing.Point(229, 89);
+            this.txtPalavra1.Name = "txtPalavra1";
+            this.txtPalavra1.Size = new System.Drawing.Size(217, 29);
+            this.txtPalavra1.TabIndex = 2;
+            // 
+            // txtPalavra2
+            // 
+            this.txtPalavra2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPalavra2.Location = new System.Drawing.Point(229, 175);
+            this.txtPalavra2.Name = "txtPalavra2";
+            this.txtPalavra2.Size = new System.Drawing.Size(217, 29);
+            this.txtPalavra2.TabIndex = 3;
+            // 
+            // btnRemover1
+            // 
+            this.btnRemover1.Location = new System.Drawing.Point(122, 283);
+            this.btnRemover1.Name = "btnRemover1";
+            this.btnRemover1.Size = new System.Drawing.Size(135, 54);
+            this.btnRemover1.TabIndex = 4;
+            this.btnRemover1.Text = "Remove Ocorrencias";
+            this.btnRemover1.UseVisualStyleBackColor = true;
+            this.btnRemover1.Click += new System.EventHandler(this.btnRemover1_Click);
+            // 
+            // bntRemover2
+            // 
+            this.bntRemover2.Location = new System.Drawing.Point(311, 283);
+            this.bntRemover2.Name = "bntRemover2";
+            this.bntRemover2.Size = new System.Drawing.Size(135, 54);
+            this.bntRemover2.TabIndex = 5;
+            this.bntRemover2.Text = "Remover Ocorrencias (replace)";
+            this.bntRemover2.UseVisualStyleBackColor = true;
+            this.bntRemover2.Click += new System.EventHandler(this.bntRemover2_Click);
+            // 
+            // btnInverte
+            // 
+            this.btnInverte.Location = new System.Drawing.Point(510, 283);
+            this.btnInverte.Name = "btnInverte";
+            this.btnInverte.Size = new System.Drawing.Size(135, 54);
+            this.btnInverte.TabIndex = 6;
+            this.btnInverte.Text = "Inverter";
+            this.btnInverte.UseVisualStyleBackColor = true;
+            this.btnInverte.Click += new System.EventHandler(this.btnInverte_Click);
+            // 
+            // frmExercicio3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInverte);
+            this.Controls.Add(this.bntRemover2);
+            this.Controls.Add(this.btnRemover1);
+            this.Controls.Add(this.txtPalavra2);
+            this.Controls.Add(this.txtPalavra1);
+            this.Controls.Add(this.lblPalavra2);
+            this.Controls.Add(this.lblPalavra1);
+            this.Name = "frmExercicio3";
+            this.Text = "frmExercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPalavra1;
+        private System.Windows.Forms.Label lblPalavra2;
+        private System.Windows.Forms.TextBox txtPalavra1;
+        private System.Windows.Forms.TextBox txtPalavra2;
+        private System.Windows.Forms.Button btnRemover1;
+        private System.Windows.Forms.Button bntRemover2;
+        private System.Windows.Forms.Button btnInverte;
+    }
+}
---
+++ Atividade 6/frmExercicio3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste_de_Metodos
+{
+    public partial class frmExercicio3 : Form
+    {
+        private void btnRemover1_Click(object sender, EventArgs e)
+        {
+            int posicao;
+
+            posicao = txtPalavra2.Text.IndexOf(txtPalavra1.Text);
+
+            while (posicao >= 0)
+            {
+                txtPalavra2.Text = txtPalavra2.Text.Substring(0, posicao) +
+                  txtPalavra2.Text.Substring(posicao + txtPalavra1.Text.Length,
+                    txtPalavra2.Text.Length - txtPalavra1.Text.Length - posicao);
+
+                posicao = txtPalavra2.Text.IndexOf(txtPalavra1.Text);
+            }
+        }
+
+        private void bntRemover2_Click(object sender, EventArgs e)
+        {
+            txtPalavra2.Text = txtPalavra2.Text.Replace(txtPalavra1.Text," ");
+        }
+
+        private void btnInverte_Click(object sender, EventArgs e)
+        {
+            string s = txtPalavra1.Text;
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+
+            foreach (char c in arr) 
+            {
+                txtPalavra2.Text += c;
+            }
+
+        }
+    }
+}
